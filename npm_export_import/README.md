3a6dc4ebc9115f8d1b4221246e39f4e7d72e8eb9
jscongdon <jason.s.congdon@gmail.com>
2026-03-13 15:11:42 -0400

    Remove npm_export_import addon files

--- npm_export_import/README.md
+++
@@ -1,5 +0,0 @@
-# My Add-on
-
-Minimal Home Assistant add-on example.
-
-Build:
--- npm_export_import/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Simple test service: serve files on 8080
-exec python3 -m http.server 8080 --bind 0.0.0.0

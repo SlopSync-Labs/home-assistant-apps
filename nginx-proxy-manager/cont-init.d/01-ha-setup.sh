c7da3e15bab618cbc29ee5863730bdd7c11ea0df
jscongdon <jason.s.congdon@gmail.com>
2026-03-13 16:19:56 -0400

    Harden NPM container init and ignore local dev artifacts

--- nginx-proxy-manager/cont-init.d/01-ha-setup.sh
+++ nginx-proxy-manager/cont-init.d/01-ha-setup.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/with-contenv bash
+#!/bin/sh
 # Runs early in s6's cont-init sequence.
 # Creates the /data sub-directories NPM expects on the HA persistent volume.
-
 mkdir -p /data/letsencrypt /data/nginx /data/logs /data/access
